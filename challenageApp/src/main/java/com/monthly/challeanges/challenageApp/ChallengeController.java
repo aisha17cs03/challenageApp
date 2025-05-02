75c69803cabb07b79e45deb1edcbdfc65a49e1a1
Aisha Tayyaba <aisha17cs03@gmail.com>
2025-05-03 03:21:54 +0530

    ControllerService file added

--- challenageApp/src/main/java/com/monthly/challeanges/challenageApp/ChallengeController.java
+++ challenageApp/src/main/java/com/monthly/challeanges/challenageApp/ChallengeController.java
@@ -1,36 +1,49 @@
 package com.monthly.challeanges.challenageApp;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 //This is a controller
 @RestController
 public class ChallengeController {
-    private List<Challenge> challenges = new ArrayList<>();
+    private ChallengeService challengeService;
 
-    //creating a constructor
-    public ChallengeController(){
-        //adding a challenge into a list
-        Challenge challenge1=new Challenge(100L, "January", "New challange i am adding here so that I can see the challanges for the user");
-        challenges.add(challenge1);
+
+    public ChallengeController(ChallengeService challengeService){
+        this.challengeService=challengeService;
+
     }
 
 
     //Creating GET request to get all challenges
     @GetMapping("/challenges")
     public List<Challenge> getAllChallenges(){
-        return challenges;
+
+        return challengeService.getAllChallenges();
     }
 
     //Creating POST request to adding a challenge by the user
     @PostMapping("/challenges")
     public String addChallenge(@RequestBody Challenge challenge){
-        challenges.add(challenge);
-        return "challenge added successfully";
+        boolean isChallangeAddded = challengeService.addChallenge(challenge);
+        if(isChallangeAddded){
+            return "challenge added successfully";
+        }
+        else{
+            return "challenge not added successfully";
+        }
+    }
+
+    //Creating Get request to get a monthly challenge
+    @GetMapping("/challenges/{month}")
+    public Challenge getMonthlyChallenges(@PathVariable String month){
+        Challenge challenge=challengeService.getMonthlyChallenges(month);
+        if(challenge != null){
+            return challenge;
+        }
+        else{
+            return null;
+        }
     }
 }
---
+++ challenageApp/src/main/java/com/monthly/challeanges/challenageApp/ChallengeService.java
@@ -0,0 +1,44 @@
+package com.monthly.challeanges.challenageApp;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ChallengeService {
+
+    private List<Challenge> challenges = new ArrayList<>();
+    private Long nextId=2L;
+
+
+    public ChallengeService(){
+        Challenge challenge1=new Challenge(1L, "January", "New challange i am adding here");
+        challenges.add(challenge1);
+    }
+
+    public List<Challenge> getAllChallenges(){
+        return challenges;
+    }
+
+    public Boolean addChallenge(@RequestBody Challenge challenge){
+        if(challenge != null) {
+            challenge.setId(nextId++);
+            challenges.add(challenge);
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public Challenge getMonthlyChallenges(String month) {
+        for(Challenge challenge: challenges){
+            if(challenge.getMonth().equalsIgnoreCase(month)){
+                return challenge;
+            }
+        }
+        return null;
+    }
+}
